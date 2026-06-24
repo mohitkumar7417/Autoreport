87f0cfea6d0a8183a439c80ba956fb76f4861829
mohitkumar7417 <mpal007417@gmail.com>
2026-06-24 16:52:49 +0530

    Delete trend_analysis.py

--- trend_analysis.py
+++
@@ -1,8 +0,0 @@
-def moving_average(df, column, window=2):
-    return df[column].rolling(window=window).mean()
-
-def growth_rate(previous_value, current_value):
-    return ((current_value - previous_value) / previous_value) * 100
-
-def cumulative_growth(series):
-    return ((series.iloc[-1] - series.iloc[0]) / series.iloc[0]) * 100
